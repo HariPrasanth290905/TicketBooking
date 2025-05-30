7006534fbc40f61f506113e602fbb8b997d39002
HariPrasanth290905 <hariprasanth4466@gmail.com>
2025-05-30 13:55:59 +0530

    Delete TicketBookFront directory

--- TicketBookFront/src/main/java/com/irah/ticketbookfront/service/TicketService.java
+++
@@ -1,9 +0,0 @@
-package com.irah.ticketbookfront.service;
-
-import com.irah.ticketbookfront.model.Passenger;
-import com.irah.ticketbookfront.model.Ticket;
-
-public interface TicketService {
-    Ticket bookTicket(Passenger passenger);
-    Ticket getTicket(int ticketNumber);
-}
